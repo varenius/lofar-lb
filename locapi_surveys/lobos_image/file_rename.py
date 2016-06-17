f719c672a7178a203d0a201c525b71bb1ad9f919
colmcoughlan <colmcoughlan@gmail.com>
2016-06-17 09:38:45 +0000

    Remove directory with old name

--- locapi_surveys/lobos_image/file_rename.py
+++
@@ -1,8 +0,0 @@
-import os,sys,glob,numpy as np
-flist = np.sort(glob.glob('SRM*.tar'))
-for f in flist:
-    newname = 'L'+f.split('L')[1].split('MS')[0]+'MS.tar'
-    os.system('mv '+f+' '+newname)
-    os.system('tar xvf '+newname)
-    os.system('rm -f '+newname)
-
